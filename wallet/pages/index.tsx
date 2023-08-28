ca07f5afe6c2507ef92290f542031ba90afcf3f4
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-08-27 23:18:22 -0300

    setup

---
+++ wallet/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tá investido</title>
+        <meta name="description" content="Sua carteira de investimento" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Tá investido</h1>
+      </div>
+    </>
+  )
+}
